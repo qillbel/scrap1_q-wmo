21694ea0a3b7679ff06bfe0192529676ab039cb2
qillbel <qillbel@gmail.com>
2025-03-01 07:47:30 +0000

    adding scripts

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from scrape import scrapePirates, scrapeWeather
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World."
+@app.route('/scrapeweather', methods=['GET'])
+def scrape_weather():
+    #scrape weather call
+    weather_data_path ='https://raw.githubusercontent.com/qillbel/scrap1_q-wmo/main/scraped_weather.csv'
+    got_df = pd.read_csv(weather_data_path)
+    result = got_df.to_json(orient='records')
+    parsed = json.loads(result)
+    resu = json.dumps(parsed)
+    return resu
+
+if __name__ == "__main__":
+    app.run()
